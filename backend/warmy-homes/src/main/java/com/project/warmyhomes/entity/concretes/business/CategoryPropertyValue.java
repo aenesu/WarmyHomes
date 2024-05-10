4be4f34778003dda629d8c5df11e0b2c0fd73d4f
Adam Agar <adamagar88@gmail.com>
2024-05-09 22:02:32 -0400

    Add Propery Values Entities

--- backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/CategoryPropertyValue.java
+++ backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/CategoryPropertyValue.java
@@ -1,4 +1,23 @@
 package com.project.warmyhomes.entity.concretes.business;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "category_property_values")
 public class CategoryPropertyValue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String value;
+
+    @Column(nullable = false)
+    private Integer advert_id;
+
+    @Column(nullable = false)
+    private Integer category_property_key_id;
+
+    // Getters and setters
 }
