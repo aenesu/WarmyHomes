743b9a83e60df3c1df8af7776fa8ee475dd8db2b
Adam Agar <adamagar88@gmail.com>
2024-05-10 15:23:39 -0400

    Add Entities for User

--- backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/user/User.java
+++ backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/user/User.java
@@ -2,7 +2,12 @@ package com.project.warmyhomes.entity.concretes.user;
 
 import lombok.*;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
 
 @Getter
 @Setter
@@ -17,5 +22,37 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Size(min = 2, max = 30)
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Size(min = 2, max = 30)
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Size(min = 10, max = 80)
+    @Email
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "phone", nullable = false, unique = true)
+    private String phone;
+
+    @Size(min = 8, max = 100)
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "reset_password_code")
+    private String resetPasswordCode;
+
+    @Column(name = "built_in", nullable = false)
+    private Boolean builtIn;
+
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
+    @Column(name = "update_at")
+    private LocalDateTime updateAt;
 
 }
