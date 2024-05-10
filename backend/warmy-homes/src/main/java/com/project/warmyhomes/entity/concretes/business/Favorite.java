3a1d753e90d2a2f333ccc9e46cc733ebf560d6e5
Adam Agar <adamagar88@gmail.com>
2024-05-09 22:07:59 -0400

    Add Entities for Favorites

--- backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/Favorite.java
+++ backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/Favorite.java
@@ -1,4 +1,28 @@
 package com.project.warmyhomes.entity.concretes.business;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "favorites")
 public class Favorite {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer user_id;
+
+    @Column(nullable = false)
+    private Integer advert_id;
+
+    @Column(nullable = false)
+    private LocalDateTime created_at;
 }
