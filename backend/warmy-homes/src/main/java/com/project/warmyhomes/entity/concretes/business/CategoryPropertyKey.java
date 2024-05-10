c66e3c40b1276e31819b9a5c3f4a958975ed965c
Adam Agar <adamagar88@gmail.com>
2024-05-09 21:39:01 -0400

    Add Entites for Property Values

--- backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/CategoryPropertyKey.java
+++ backend/warmy-homes/src/main/java/com/project/warmyhomes/entity/concretes/business/CategoryPropertyKey.java
@@ -1,4 +1,29 @@
 package com.project.warmyhomes.entity.concretes.business;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "category_property_keys")
 public class CategoryPropertyKey {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Size(min = 2, max = 200)
+    private String name;
+
+    @Column(nullable = false, columnDefinition = "boolean default false") 
+    private Boolean built_in;
+
+    @Column(nullable = false)
+    private Integer category_id;
 }
