b0713da80ceb9cdcbe5d58497f709c48a400977e
hikmet <hikmetvural13@gmail.com>
2024-05-16 16:16:17 -0500

    WarmyHomes logo inserted, slogan written, icons inserted

--- frontend/warmy-homes/components/common/footer/footer.jsx
+++ frontend/warmy-homes/components/common/footer/footer.jsx
@@ -1,14 +1,31 @@
 import styles from "./footer.module.scss"
-import { FaGooglePlay } from "react-icons/fa";
+import { FaAppStore, FaGooglePlay } from "react-icons/fa";
 
 
 export default function Footer() {
   return (
     <div className={styles.divClass} >
       
-      footer here
-
+      <div className={styles.footerSegment}>
+      <img src="/assets/images/logo-white2.png"></img>
+      <p>Our slogan, 'Unlocking Your Home's Potential, Together,' reflects our dedication to helping clients realize the full potential of their real estate investments through collaborative and personalized services. </p>
+      <div>
+        <div className="button">
+        <FaAppStore />
+        </div>
+        <div className="button">
+        <FaGooglePlay />
+        </div>
+      </div>
+      </div>
+      <div className={styles.footerSegment}> 
+      
+      </div>
+      <div className={styles.footerSegment}>
       <FaGooglePlay />
+      </div>
+
+      
 
     </div>
   )
