0bc14abb7c5f768f2658d409b9a549f94a77d06e
Ayseakkurt <akkurtyes@gmail.com>
2024-05-17 17:37:07 -0600

    Header

--- frontend/warmy-homes/components/common/header/header.jsx
+++ frontend/warmy-homes/components/common/header/header.jsx
@@ -1,7 +1,37 @@
-import styles from "./header.module.scss";
+import styles from './header.module.scss';
+import AddPropertyButton from './addPropertyButton/addPropertyButton';
+import { AiOutlineUser } from 'react-icons/ai';
 
-export default function Header() {
+const Header = () => {
   return (
-    <div>header date</div>
-  )
-}
+    <div className={styles.headerContainer}>
+      <div className={styles.logo}>
+        <img src="/assets/images/logo-white2.png" alt="Logo" />
+      </div>
+      <div className={styles.navContainer}>
+        <ul className={styles.navbarNav}>
+          <li>
+            <a className="nav-link" href='/#'>Home</a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#">Properties</a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#">About</a>
+          </li>
+          <li>
+            <a className="nav-link" href="/#">Contact</a>
+          </li>
+        </ul>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <button className={styles.loginButton}>
+          <AiOutlineUser className={styles.icon} /> Login/Register
+        </button>
+        <AddPropertyButton />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
