441ce22ffe95e4073fb368015a5bbf9e3f194665
Ayseakkurt <akkurtyes@gmail.com>
2024-05-18 20:08:14 -0600

    Added padding for button

--- frontend/warmy-homes/components/common/header/header.jsx
+++ frontend/warmy-homes/components/common/header/header.jsx
@@ -1,32 +1,35 @@
 import styles from './header.module.scss';
 import AddPropertyButton from './addPropertyButton/addPropertyButton';
 import { AiOutlineUser } from 'react-icons/ai';
+import { Akshar } from 'next/font/google'; // Ensure correct import if using next/font
+
+const akshar = Akshar({ subsets: ["latin"], weight: ["300", "400", "500", "600", "700"] });
 
 const Header = () => {
   return (
-    <div className={styles.headerContainer}>
+    <div className={`${styles.headerContainer} ${akshar.className}`}>
       <div className={styles.logo}>
         <img src="/assets/images/logo-white2.png" alt="Logo" className={styles.logoImage} />
       </div>
       <div className={styles.navContainer}>
         <ul className={styles.navbarNav}>
           <li>
-            <a className="nav-link" href='/#'>Home</a>
+            <a className={`${styles.navLink} ${akshar.className}`} href='/#'>Home</a>
           </li>
           <li>
-            <a className="nav-link" href="/#">Properties</a>
+            <a className={`${styles.navLink} ${akshar.className}`} href="/#">Properties</a>
           </li>
           <li>
-            <a className="nav-link" href="/#">About</a>
+            <a className={`${styles.navLink} ${akshar.className}`} href="/#">About</a>
           </li>
           <li>
-            <a className="nav-link" href="/#">Contact</a>
+            <a className={`${styles.navLink} ${akshar.className}`} href="/#">Contact</a>
           </li>
         </ul>
       </div>
       <div className={styles.buttonsContainer}>
-        <button className={styles.loginButton}>
-          <AiOutlineUser className={styles.icon} /> Login/Register
+        <button className={`${styles.loginButton} ${akshar.className}`}>
+          <AiOutlineUser className={styles.icon} /> Login&nbsp;/&nbsp;Register
         </button>
         <AddPropertyButton />
       </div>
