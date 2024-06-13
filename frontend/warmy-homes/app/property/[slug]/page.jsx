a87e9273a30ff70c79c9e35d55df4666ec901341
aenesu <aenesu@outlook.com>
2024-06-13 15:04:42 +0300

    Properity Details Page created

--- frontend/warmy-homes/app/property/[slug]/page.jsx
+++ frontend/warmy-homes/app/property/[slug]/page.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import styles from './properity.module.scss';
 
 export default function RandomProp() {
+
+  const advert = {
+    id: 'home',
+    title: 'ev',
+    desc: '',
+    slug: '',
+    price: '',
+    status: '',
+    built_in: '',
+    is_active: '',
+    view_count: '',
+    location: '',
+    advert_type_id: '',
+    country_id: '',
+    city_id: '',
+    district_id: '',
+    user_id: '',
+    category_id: '',
+    create_at: '',
+    update_at: '',
+  }
+
   return (
-    <div>RandomProp</div>
+    <div className={styles.container}>
+
+      <div className={styles.banner}>
+        <div className={styles.title}>
+          {advert.title}
+        </div>
+      </div>
+
+      <div className={styles.content}>
+        <div className={styles.left}>
+          <div className={styles.photoContainer}>
+            fotoğraflar
+          </div>
+
+          <div className={styles.detailsContainer}>
+          </div>
+
+          <div className={styles.locationContainer}>
+          </div>
+        </div>
+
+        <div className={styles.right}>
+          <div className={styles.tourContainer}>
+            places
+          </div>
+        </div>
+      </div>
+
+    </div>
   )
 }
