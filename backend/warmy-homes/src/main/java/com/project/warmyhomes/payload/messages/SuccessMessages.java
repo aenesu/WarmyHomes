12f148d49e344560f9cbd61ec1a7d5e170a57690
erendibek576 <erendibek576@gmail.com>
2024-06-27 19:51:57 +0300

    User endpoints are completed

--- backend/warmy-homes/src/main/java/com/project/warmyhomes/payload/messages/SuccessMessages.java
+++ backend/warmy-homes/src/main/java/com/project/warmyhomes/payload/messages/SuccessMessages.java
@@ -7,4 +7,5 @@ public class SuccessMessages {
     public static final String USER_CREATE = "User is  saved successfully";
     public static final String USER_FOUND = "User is found successfully";
     public static final String USER_UPDATE = "User is updated successfully";
+    public static final String USER_DELETE = "User is deleted successfully";
 }
