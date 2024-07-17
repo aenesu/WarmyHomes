608765cf538b6a3f5ddf420a9ee7e57c7bab394a
aenesu <aenesu@outlook.com>
2024-07-17 20:23:05 +0300

    Minor fixes

--- frontend/warmy-homes/components/common/landing-page/discover-popular-properties/discover-popular-properties.jsx
+++ frontend/warmy-homes/components/common/landing-page/discover-popular-properties/discover-popular-properties.jsx
@@ -1,6 +1,7 @@
 import styles from "./discover-popular-properties.module.scss";
 
 import AdvertCard from "../../advert-card/advert-card";
+import Link from "next/link";
 
 export default function DiscoverPopularProperties() {
 
@@ -19,7 +20,7 @@ export default function DiscoverPopularProperties() {
       <div className={styles.exploreText}>Featured properties</div>
       <div className={styles.categoryCards}>
         {adverts.map(({ id, title, city, district, price }) => (
-          < AdvertCard key={id} {...{ title, city, district, price }} />
+          <Link key={id} href="/properties/new-york-home">< AdvertCard key={id} {...{ title, city, district, price }} /></Link>
         ))}
       </div>
     </div>
