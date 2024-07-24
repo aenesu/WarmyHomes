735c43764db3a80fe8b9f3f93fd905152b9e74df
hikmet <hikmetvural13@gmail.com>
2024-07-24 16:11:50 -0500

    admincategories page done

--- frontend/warmy-homes/components/admin/categories-card/categories-card.jsx
+++ frontend/warmy-homes/components/admin/categories-card/categories-card.jsx
@@ -10,34 +10,28 @@ export default function CategoriesCard({
 })
 
 {
-    return (
-        <div className={styles.card}>
-          <div className={styles.container}>
-            <img
-              src="/assets/vectors/house.svg"
-              alt="House Icon"
-              className={styles.icon} 
-            />
-            <div className={styles.details}>
-              <div className={styles.name}>{name}</div>
-              <div className={styles.sequence}>{sequence}</div>
-            </div>
-            <div className={styles.active}>
-              {active ? (
-                  <img src="/assets/vectors/check-mark.svg" alt="Active" />
-                ) : (
-                  <img src="/assets/vectors/cross.svg" alt="Inactive" />
-                )}
-            </div>
-            <div className={styles.action}>
-              <button className={styles.editButton}>
-                <img src="/assets/vectors/bin.svg" alt="Delete Symbol" />
-              </button>
-              <button className={styles.editButton}>
-                <img src="/assets/vectors/purpedit.svg" alt="Edit Symbol" />
-              </button> 
-            </div>
-          </div>
-        </div>
-      );
+  return (
+    <div className={styles.card}>
+      <div className={styles.icon}>
+        <img src={`/assets/vectors/${icon}`} alt="Category Icon" />
+      </div>
+      <div className={styles.name}>{name}</div>
+      <div className={styles.sequence}>{sequence}</div>
+      <div className={styles.active}>
+        {active ? (
+          <img src="/assets/vectors/check-mark.svg" alt="Active" />
+        ) : (
+          <img src="/assets/vectors/cross.svg" alt="Inactive" />
+        )}
+      </div>
+      <div className={styles.action}>
+        <button className={styles.editButton}>
+          <img src="/assets/vectors/bin.svg" alt="Delete Symbol" />
+        </button>
+        <button className={styles.editButton}>
+          <img src="/assets/vectors/purpedit.svg" alt="Edit Symbol" />
+        </button>
+      </div>
+    </div>
+  );
 }
