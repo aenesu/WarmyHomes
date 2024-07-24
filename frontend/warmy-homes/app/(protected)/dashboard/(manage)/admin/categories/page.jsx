4ddd0c2b63dca7ab2caa5d18f9fc7b2eade3e810
hikmet <hikmetvural13@gmail.com>
2024-07-24 15:24:21 -0500

    content ready

--- frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/page.jsx
+++ frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/page.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+"use client"
+import CategoriesCard from "@/components/admin/categories-card/categories-card";
+import styles from "./categories.module.scss";
+import { useState } from "react";
 
-export default function Categories() {
+export default function AdminCategories() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const Categories = [
+    { id: 1, icon: "house.svg", name: "Villa", sequence: 15, active: true },
+    { id: 2, icon: "house.svg", name: "Villa", sequence: 15, active: true },
+    { id: 3, icon: "house.svg", name: "Villa", sequence: 15, active: true },
+    { id: 4, icon: "house.svg", name: "Villa", sequence: 15, active: true },
+  ];
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+
   return (
-    <div>Categories</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="Type something"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className={styles.searchInput}
+        />
+        <button className={styles.searchButton}>
+          <img src="/assets/vectors/magnify.svg" alt="Search" />
+        </button>
+        <button className={styles.addButton}>ADD NEW</button>
+      </div>
+      <div className={styles.header}>
+        <div>Icon</div>
+        <div>Name</div>
+        <div>Sequence</div>
+        <div>Active</div>
+        <div>Action</div>
+      </div>
+      <div className={styles.cards}>
+        {Categories.map(({ id, icon, name, sequence, active }) => (
+          <CategoriesCard
+            key={id}
+            icon={icon}
+            name={name}
+            sequence={sequence}
+            active={active}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ frontend/warmy-homes/components/admin/categories-card/categories-card.jsx
@@ -0,0 +1,43 @@
+import styles from "./categories-card.module.scss"
+
+export default function CategoriesCard({
+    index,
+    icon,
+    name,
+    sequence,
+    active
+    
+})
+
+{
+    return (
+        <div className={styles.card}>
+          <div className={styles.container}>
+            <img
+              src="/assets/vectors/house.svg"
+              alt="House Icon"
+              className={styles.icon} 
+            />
+            <div>
+              <div className={styles.name}>{name}</div>
+              <div className={styles.sequence}>{sequence}</div>
+              <div className={styles.active}>{active}
+              {active ? (
+                  <img src="/assets/vectors/check-mark.svg" alt="Active" />
+                ) : (
+                  <img src="/assets/vectors/cross.svg" alt="Inactive" />
+                )}
+              </div>
+            </div>
+          </div>
+          <div className={styles.action}>
+            <button className={styles.editButton}>
+              <img src="/assets/vectors/bin.svg" alt="Delete Symbol" />
+            </button>
+            <button className={styles.editButton}>
+              <img src="/assets/vectors/purpedit.svg" alt="Edit Symbol" />
+            </button> 
+          </div>
+        </div>
+      );
+}
