0026c3663c395a32204b9047c321267413f7c9eb
Ayseakkurt <akkurtyes@gmail.com>
2024-07-23 20:04:46 -0600

    Completed tour request page

--- frontend/warmy-homes/app/(protected)/(user)/profile/tour-requests/page.jsx
+++ frontend/warmy-homes/app/(protected)/(user)/profile/tour-requests/page.jsx
@@ -1,17 +1,17 @@
-import MyFavoritesCard from "@/components/user-panel/my-favorites-card/my-favorites-card";
 import Banner from "@/components/common/banner/banner";
 import styles from "./tour-request.module.scss";
+import MyTourRequestCard from "@/components/user-panel/my-tour-request-card/my-tour-card";
 
 const MyTourRequest = () => {
     const myAdverts = [
-        {id: 1, title: "Equestrian Family Home", country_id:"USA", city_id:"CA", district_id:"California City", price: 1400, category_id:"John Doe", advert_type_id:"10/10/2023", time_id: "10:30PM"},
-        {id: 1, title: "Equestrian Family Home", country_id:"USA", city_id:"CA", district_id:"California City", price: 1400, category_id:"John Doe", advert_type_id:"10/10/2023", time_id: "10:30PM" }, 
+        {id: 1, title: "Equestrian Family Home", country_id:"USA", city_id:"CA", district_id:"California City", price: 1400, category_id:"John Doe",  status:"Pending", advert_type_id:"10/10/2023", time_id: "10:30 PM"},
+        {id: 1, title: "Equestrian Family Home", country_id:"USA", city_id:"CA", district_id:"California City", price: 1400, category_id:"John Doe",  status:"Pending", advert_type_id:"10/10/2023", time_id: "10:30 PM" }, 
     ];
 
     return (
         <div className={styles.mainContainer}>
         <div className={styles.container}>
-            <Banner title="MY TOUR REQUEST" />
+            <Banner title="MY TOUR REQUESTS" />
             <div className={styles.words}>
                 <h4>Property</h4>
                 <h4>Owner</h4>
@@ -22,8 +22,8 @@ const MyTourRequest = () => {
             </div>
             
             <div className={styles.cards}>
-                {myAdverts.map(({id, title, country_id, city_id, district_id, category_id, advert_type_id, price, time_id}) => (
-                    <MyFavoritesCard key={id} {...{title, country_id, city_id, district_id, category_id, advert_type_id, price, time_id }} />
+                {myAdverts.map(({id, title, country_id, city_id, district_id, category_id, advert_type_id, price, status, time_id}) => (
+                    <MyTourRequestCard key={id} {...{title, country_id, city_id, district_id, category_id, advert_type_id, status, price, time_id }} />
                     
                 ))}
                 
---
+++ frontend/warmy-homes/components/user-panel/my-tour-request-card/my-tour-card.jsx
@@ -0,0 +1,42 @@
+import styles from "./my-tour-card.module.scss";
+
+export default function MyTourRequestCard({
+  index,
+  title,
+  country_id,
+  city_id,
+  district_id,
+  category_id,
+  advert_type_id,
+  status,
+  price,
+  time_id 
+}) {
+  return (
+    <div className={styles.card}>
+      <div className={styles.container}>
+        <img
+          src="/assets/images/house-isolated-field 6.png"
+          alt={title}
+          className={styles.image}
+        />
+        <div>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.location}>{district_id}, {city_id}, {country_id}</div>
+          <div className={styles.price}>$ {price.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</div>
+        </div>
+      </div>
+      <div className={styles.category}>{category_id}</div>
+      <div className={styles.status}>{status}</div>
+      <div className={styles.type}>{advert_type_id}</div>
+      
+      {time_id && <div className={styles.time}>{time_id}</div>}
+      <div className={styles.action}>
+        <button className={styles.actButton}><img src="/assets/vectors/bin.svg" alt="Rubbish Symbol"></img></button> 
+      </div>
+      <div className={styles.action}>
+        <button className={styles.editButton}><img src="/assets/vectors/editP.svg" alt="Edit Symbol"></img></button> 
+      </div>
+    </div>
+  );
+}
