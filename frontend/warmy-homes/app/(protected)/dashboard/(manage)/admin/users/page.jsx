79d61605b99ea9a06ea6dfa0d1436e729dbbb44a
hikmet <hikmetvural13@gmail.com>
2024-09-02 15:21:48 -0500

    admin users done

--- frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/users/page.jsx
+++ frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/users/page.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+"use client";
+import UsersCard from "@/components/admin/admin-users-card/admin-users-card";
+import styles from "./admin-users.module.scss";
+import { useState } from "react";
 
-export default function Users() {
+export default function AdminUsers() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const Users = [
+    { id: 1, name: "Aaron Hank", email: "example@example.com", phone: "(454)454 45 45" },
+    { id: 2, name: "Aaron Hank", email: "example@example.com", phone: "(454)454 45 45" },
+    { id: 3, name: "Aaron Hank", email: "example@example.com", phone: "(454)454 45 45" },
+    { id: 4, name: "Aaron Hank", email: "example@example.com", phone: "(454)454 45 45" },
+  ];
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
-    <div>Users</div>
-  )
+    <div className={styles.container}>
+      <div className={styles.searchBar}>
+        <input
+          type="text"
+          placeholder="Type something"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          className={styles.searchInput}
+        />
+        <button className={styles.searchButton}>
+          <img src="/assets/vectors/magnify.svg" alt="Search" />
+        </button>
+      </div>
+      <div className={styles.header}>
+        <div>Name</div>
+        <div>Email</div>
+        <div>Phone</div>
+        <div>Actions</div>
+      </div>
+      <div className={styles.cards}>
+        {Users.map(({ id, name, email, phone }) => (
+          <UsersCard
+            key={id}
+            name={name}
+            email={email}
+            phone={phone}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
