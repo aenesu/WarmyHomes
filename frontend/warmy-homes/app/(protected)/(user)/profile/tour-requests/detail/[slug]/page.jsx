051c5eaa6a5fc68bd91df2a0bd2a038738e61bc8
Ayseakkurt <akkurtyes@gmail.com>
2024-09-04 14:59:20 -0600

    Added the banner, the picture, the boxes, and buttons for page

--- frontend/warmy-homes/app/(protected)/(user)/profile/tour-requests/detail/[slug]/page.jsx
+++ frontend/warmy-homes/app/(protected)/(user)/profile/tour-requests/detail/[slug]/page.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import styles from "./tour-requests-detail.module.scss";
+import Banner from "@/components/common/banner/banner";
+import Image from "next/image";
 
 export default function TourDetails() {
   return (
-    <div>TourDetails</div>
-  )
+    <div className={styles.container}>
+      <Banner title={"MY TOUR REQUESTS"} />
+      <div className={styles.content}>
+        <div className={styles.imageContainer}>
+          <Image 
+            src="/assets/images/house-isolated-field 6.png" 
+            width={500} 
+            height={300} 
+            alt="House in isolated field" 
+          />
+          <div className={styles.statusBox}>Pending</div>
+        </div>
+        <div className={styles.detailsContainer}>
+          <h5 className={styles.title}>Equestrian Family Home</h5>
+          <p className={styles.location}>California City, CA, USA</p>
+          <span className={styles.price}>$1400.00</span>
+          
+          <div className={styles.form}>
+            <div className={styles.formGroup}>
+              <label htmlFor="tourDate">Tour Date</label>
+              <input type="date" id="tourDate" className={styles.inputBox} />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="tourTime">Tour Time</label>
+              <input type="time" id="tourTime" className={styles.inputBox} />
+            </div>
+          </div>
+
+          <div className={styles.buttonContainer}>
+            <button className={styles.cancelButton}>Cancel</button>
+            <button className={styles.updateButton}>Update</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
