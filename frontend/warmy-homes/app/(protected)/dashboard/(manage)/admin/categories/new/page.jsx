3f0eb20f0a59472d34da8e39ae284c496df741b5
aenesu <aenesu@outlook.com>
2024-09-05 22:29:41 +0300

    last page coded :(

--- frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/new/page.jsx
+++ frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/new/page.jsx
@@ -1,7 +1,74 @@
-import React from 'react'
+"use client";
+import { useState } from 'react';
+import styles from './new.module.scss';
 
-export default function NewCategory() {
+export default function CategoryEdit() {
+  const [status, setStatus] = useState('Active');
+
+  const toggleStatus = () => {
+    setStatus(prevStatus => (prevStatus === 'Active' ? 'Deactive' : 'Active'));
+  };
+
   return (
-    <div>NewCategory</div>
-  )
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <div className={styles.sides}>
+          <div className={styles.side}>
+
+            {/* Title and Slug */}
+            <div className={styles.formRowWithProperties}>
+              <div className={`${styles.inputField} ${styles.leftAligned}`}>
+                <label>Title</label>
+                <input type="text" required />
+              </div>
+
+              <div className={`${styles.inputField} ${styles.leftAligned}`}>
+                <label>Slug</label>
+                <input type="text" required />
+              </div>
+            </div>
+
+            {/* Icon, Sequence, and Status */}
+            <div className={styles.formRow}>
+              <div className={`${styles.inputField} ${styles.small}`}>
+                <label>Icon</label>
+                <input type="text" required />
+              </div>
+
+              <div className={`${styles.inputField} ${styles.small}`}>
+                <label>Sequence</label>
+                <input type="text" required />
+              </div>
+
+              <div className={styles.statusContainer}>
+                <label>Status</label>
+                <button type="button" className={styles.statusButton} onClick={toggleStatus}>
+                  <span>{status}</span>
+                  <div className={`${styles.statusIndicator} ${status === 'Active' ? styles.active : ''}`}></div>
+                </button>
+
+              </div>
+
+            </div>
+
+
+            {/* Button Group */}
+            <div className={styles.buttonGroup}>
+              <button type="submit" className={styles.saveButton}>Create</button>
+            </div>
+          </div>
+
+          <div className={styles.side}>
+            {/* Property Container */}
+            <div className={styles.propertyContainer}>
+              <h3 className={styles.propertyTitle}>Properties</h3>
+              <div className={styles.addPropertyBox}></div>
+              <div className={styles.propertyItem}>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
