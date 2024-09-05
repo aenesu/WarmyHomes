6eafb5676c0910abba7fa8cf2b2895f4cdb6bb75
Ayseakkurt <akkurtyes@gmail.com>
2024-09-04 23:50:35 -0600

    edit text input boxes created

--- frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/edit/[slug]/page.jsx
+++ frontend/warmy-homes/app/(protected)/dashboard/(manage)/admin/categories/edit/[slug]/page.jsx
@@ -1,7 +1,47 @@
-import styles from "./edit.module.scss"
+"use client";
+import { useState } from 'react';
+import styles from './edit.module.scss';
 
 export default function CategoryEdit() {
+  const [status, setStatus] = useState('Active');
+
+  const toggleStatus = () => {
+    setStatus(prevStatus => (prevStatus === 'Active' ? 'Deactive' : 'Active'));
+  };
+
   return (
-    <div>CategoryEdit</div>
-  )
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <div className={styles.inputField}>
+          <label>Title</label>
+          <input type="text" required />
+        </div>
+
+        <div className={styles.inputField}>
+          <label>Slug</label>
+          <input type="text" required />
+        </div>
+
+        <div className={styles.formRow}>
+          <div className={`${styles.inputField} ${styles.small}`}>
+            <label>Icon</label>
+            <input type="text" required />
+          </div>
+
+          <div className={`${styles.inputField} ${styles.small}`}>
+            <label>Sequence</label>
+            <input type="text" required />
+          </div>
+
+          <div className={styles.statusContainer}>
+            <label>Status</label>
+            <button type="button" className={styles.statusButton} onClick={toggleStatus}>
+              <span>{status}</span>
+              <div className={`${styles.statusIndicator} ${status === 'Active' ? styles.active : ''}`}></div>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
